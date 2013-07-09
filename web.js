b06cfd0352851eae2d51c45072a746114befff3a
Ubuntu <ubuntu@ip-172-31-0-195.us-west-2.compute.internal>
2013-07-09 16:02:29 +0000

    flereadsync

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buffer = fs.readFile("index.html");
+  buffer = fs.readFileSync("index.html");
   response.send(buffer.toString());
 });
 
